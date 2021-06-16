9ae9382a4038067857ae6fa8e08b548bc2174687
Abhishek Kumar <abhishek020898@gmail.com>
2021-06-16 14:13:53 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <h1>Portfolio project...</h1>
+    </div>
+  );
+}
+
+export default App;
